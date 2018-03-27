e693d3770c6638ea625005a0a6c9b324dde4f0b2
Arthur Comben <anthillape@gmail.com>
2018-03-27 23:01:37 +0100

    Set up some initial linting rules

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "prettier"
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true,
+      "jsx": true
+    },
+    "sourceType": "module"
+  },
+  "plugins": [
+    "prettier",
+    "react"
+  ],
+  "rules": {
+    "prettier/prettier": "error",
+    "indent": [
+      "error",
+      "tab"
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  }
+};
---
+++ src/components/layout/layout.jsx
@@ -0,0 +1,11 @@
+import styles from 'layout.css';
+
+const layout = () => (
+  <div class={ styles.layout }>
+    <div class={ styles.world }>
+    </div>
+    <div class={ styles.controls }>
+    </div>
+  </div>
+);
+
